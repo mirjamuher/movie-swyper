db5eaa1ae8b107b03e0fea562898a501f4a561ab
Mirjam Uher <uher.mirjam@gmail.com>
2021-04-11 17:49:34 +1000

    first draft genre page, using TMDb API

--- project/movieswyper/views/select.py
+++ project/movieswyper/views/select.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 
+from project.settings import TMDB_API_KEY
+import requests
+
+
 # add login later
 def genres(request):
-    context = {}
-    return render(request, 'movieswyper/select_genres.html', context)
+    response = requests.get(f"https://api.themoviedb.org/3/genre/movie/list?api_key={TMDB_API_KEY}&language=en-US")
+    if response.status_code == 200:
+        info_json = response.json()
+    return render(request, 'movieswyper/select_genres.html', {
+        'genres': info_json['genres'],
+    })
+
+"""
+Genre info:
+{"genres":[
+{"id":28,"name":"Action"},
+{"id":12,"name":"Adventure"},
+{"id":16,"name":"Animation"},
+{"id":35,"name":"Comedy"},
+{"id":80,"name":"Crime"},
+{"id":99,"name":"Documentary"},
+{"id":18,"name":"Drama"},
+{"id":10751,"name":"Family"},
+{"id":14,"name":"Fantasy"},
+{"id":36,"name":"History"},
+{"id":27,"name":"Horror"},
+{"id":10402,"name":"Music"},
+{"id":9648,"name":"Mystery"},
+{"id":10749,"name":"Romance"},
+{"id":878,"name":"Science Fiction"},
+{"id":10770,"name":"TV Movie"},
+{"id":53,"name":"Thriller"},
+{"id":10752,"name":"War"},
+{"id":37,"name":"Western"}
+]}
+"""
 
 # add login later
 def movies(request):
