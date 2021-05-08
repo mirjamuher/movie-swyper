9a286ba23ae7ec9a0024f6bdaaaf46102dcc8c48
Mirjam Uher <uher.mirjam@gmail.com>
2021-05-08 13:40:47 +1000

    played around with customising admin site

--- project/movieswyper/admin.py
+++ project/movieswyper/admin.py
@@ -1,10 +1,18 @@
+"""
+Edit abilities of admin view: https://docs.djangoproject.com/en/3.2/ref/contrib/admin/#django.contrib.admin.register
+Will be convenient for users later on!
+"""
+
 from django.contrib import admin
 from .models import Movie, Genre, Profile, ProductionCompany, UserMovieRating
 
-# Register your models here.
-
-admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(Profile)
 admin.site.register(ProductionCompany)
 admin.site.register(UserMovieRating)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'popularity', 'vote_average')
+    ordering = ('vote_average', 'popularity')
+    search_fields = ('title', 'genres__name', 'productioncompany__name')
