9206a148ae5fce7d17f406c47a8257a3c8253339
Mirjam Uher <uher.mirjam@gmail.com>
2021-05-10 11:52:57 +1000

    made genre-card selectable

--- project/movieswyper/static/movieswyper/js/select_genres.js
+++ project/movieswyper/static/movieswyper/js/select_genres.js
@@ -7,11 +7,14 @@ function setupPage() {
 
 function chooseGenre(event) {
     const elGenre = event.currentTarget;
+    const elCheckbox = elGenre.querySelector('input[type=checkbox]')
 
     if (elGenre.classList.contains("selected")) {
         elGenre.classList.remove("selected");
+        elCheckbox.checked = false;
     } else {
         elGenre.classList.add("selected");
+        elCheckbox.checked = true;
     }
 }
 
